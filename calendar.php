b9a5acc9d26da7b06b66bb76e27b78386bbe6c6a
shane <canfield1@kenyon.edu>
2021-03-13 19:19:06 -0600

    cleaning up, adding calendar skeleton

--- calendar.php
+++ calendar.php
@@ -16,14 +16,91 @@ href= "./assets/CSS/calendar.css">
 <title>Calendar</title>
 </head>
 <body>
+
+
     <!-- stuff that will show all the time -->
-    Login:<input type = text id=username placeholder=”Username”><input type = text id=password placeholder=”Password”><button id=”login”>Log in</button>
+    Login:<input type = text id=username placeholder='Username'><input type = text id=password placeholder='Password'><button id='login'>Log in</button>
 
-    New User:<input type = text id=new_username placeholder=”Username”><input type = text id=new_password placeholder=”Password”><button id=”register”>Register</button>
+    New User:<input type = text id=new_username placeholder='Username'><input type = text id=new_password placeholder='Password'><button id='register'>Register</button>
 
-    <button id=”next_month_btn>Next Month</button>
-    <button id=”prev_month_btn>Previous Month</button>
+    <button id='next_month_btn'>Next Month</button>
+    <button id='prev_month_btn'>Previous Month</button>
 
+    <table id=Calendar style="border: 1px solid black;">
+    <h2>Current Month: <h2>
+    <tr>
+      <th>Sunday</th>
+      <th>Monday</th>
+      <th>Tuesday</th>
+      <th>Wednesday</th>
+      <th>Thursday</th>
+      <th>Friday</th>
+      <th>Saturday</th>
+    </tr>
+
+<script>
+
+    let currentMonth = new Month(2017, 9);
+
+    let weekdays = {
+        'Sunday':1,
+        'Monday':2,
+        'Tuesday':3,
+        'Wednesday':4,
+        'Thursday':5,
+        'Friday':6,
+        'Saturday':7
+    };
+
+    let cal_weeks = ['first','second','third','fourth'];
+
+    function populateCalendar(){
+
+        let calendar = document.getElementById('Calendar'); //get calendar table
+        let currentMonth = new Month(2021, 2); // March 2021
+        let weeks = currentMonth.getWeeks();
+        let date_num = 1;
+        let cal_week = 0;
+        for(var w in weeks){
+            let week_id = cal_weeks[cal_week];
+            var days = weeks[w].getDates();
+            // days contains normal JavaScript Date objects.
+            alert("Week starting on "+days[0]);
+            let startDay = weekdays[days[0]];
+            calendar.innerHTML += '<tr>';
+            for (i = 0; i < startDay; i++){
+                calendar.innerHTML += '<td>$nbsp;</td>';
+                for(var d in days){
+                    calendar.innerHTML += '<td>';
+                    calendar.innerHTML += '<h5 id='+week_id+'>'+date_num+'</h5>';
+                    calendar.innerHTML += '</td>';
+                    date_num += 1;
+                //Search SQL for events on this specific day
+                }
+        calendar.innerHTML += '</tr>';
+          }
+        }
+      }
+    // Change the month when the "next" button is pressed
+    document.getElementById("next_month_btn").addEventListener("click", function(event){
+        currentMonth = currentMonth.nextMonth();
+        calendar.innerHTML = ''; //clears calendar
+        updateCalendar();
+        alert("The new month is "+currentMonth.month+" "+currentMonth.year);
+    }, false);
+
+    // Change the month when the "previous" button is pressed
+    document.getElementById("prev_month_btn").addEventListener("click", function(event){
+        currentMonth = currentMonth.prevMonth();
+        calendar.innerHTML = ''; //clears calendar
+        updateCalendar();
+        alert("The new month is "+currentMonth.month+" "+currentMonth.year);
+    }, false);
+
+    document.addEventListener("DOMContentLoaded", populateCalendar, false);
+
+</script>
+</table>
 
     <div class="calendar-event-editor" style="display:none;"> <!-- Pop-Up For New Event -->
         Title:<input type="text" name = "title"/><br>
@@ -72,79 +149,7 @@ href= "./assets/CSS/calendar.css">
       </div>
 
 
-    <script>
-        let weekdays = {
-            “Sunday”:1,
-            “Monday”:2,
-            “Tuesday”:3,
-            “Wednesday”:4,
-            “Thursday”:5,
-            “Friday”:6,
-            “Saturday”:7
-        };
 
-        let cal_weeks = [“first”,”second”,”third”,”fourth”];
-
-        function populateCalendar(){
-
-            let calendar = document.getElementById(“Calendar”); //get calendar table
-
-            let currentMonth = new Month(2021, 2); // March 2021
-
-            let weeks = currentMonth.getWeeks();
-
-            let date_num = 1;
-            let cal_week = 0;
-
-            for(var w in weeks){
-            
-                let week_id = cal_weeks[cal_week]; 
-
-                var days = weeks[w].getDates();
-                // days contains normal JavaScript Date objects.
-                
-                alert("Week starting on "+days[0]);
-
-                let startDay = weekdays[days[0]];
-                
-                calendar.innerHTML += “<tr>”;
-                
-                for (i = 0; i < startDay; i++){
-                    calendar.innerHTML += “<td>$nbsp;</td>”;
-        
-                    for(var d in days){
-                        calendar.innerHTML += “<td>”;
-                        calendar.innerHTML += “<h5 id=”+week_id+”>”+date_num+”</h5>”;
-                        
-                        calendar.innerHTML += “</td>”;
-                        date_num += 1;
-
-                    //Search SQL for events on this specific day
-
-                    }
-		        calendar.innerHTML = “</tr>”;
-	            }
-            }
-
-        // Change the month when the "next" button is pressed
-        document.getElementById("next_month_btn").addEventListener("click", function(event){
-            currentMonth = currentMonth.nextMonth();
-            calendar.innerHTML = “”; //clears calendar
-            updateCalendar();
-            alert("The new month is "+currentMonth.month+" "+currentMonth.year);
-        }, false);
-
-        // Change the month when the "previous" button is pressed
-        document.getElementById("prev_month_btn").addEventListener("click", function(event){
-            currentMonth = currentMonth.prevMonth();
-            calendar.innerHTML = “”; //clears calendar
-            updateCalendar();
-            alert("The new month is "+currentMonth.month+" "+currentMonth.year);
-        }, false);
-
-        document.addEventListener("DOMContentLoaded", populateCalendar, false);
-
-    </script>
 
 
 
